6a18b7bf63d9ebeff3e2c6318d0569ad92339c21
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-01-10 16:52:56 +0500

    Updates

---
+++ precache-manifest.6714985c9f7bc071b9f5c9d803adb5e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8eb2f4f0ceec0f3be4d",
+    "url": "/face-recognition-app/static/css/main.5d1ecdc9.chunk.css"
+  },
+  {
+    "revision": "e8eb2f4f0ceec0f3be4d",
+    "url": "/face-recognition-app/static/js/main.e8eb2f4f.chunk.js"
+  },
+  {
+    "revision": "4fba0344a997b437fbe0",
+    "url": "/face-recognition-app/static/js/1.4fba0344.chunk.js"
+  },
+  {
+    "revision": "6de2bf8da835eb85162e",
+    "url": "/face-recognition-app/static/js/runtime~main.6de2bf8d.js"
+  },
+  {
+    "revision": "d2386874a4bf39a6181638faf3986c4d",
+    "url": "/face-recognition-app/static/media/vision.d2386874.svg"
+  },
+  {
+    "revision": "11477045d2d89f6c1620f69f29470160",
+    "url": "/face-recognition-app/index.html"
+  }
+];
